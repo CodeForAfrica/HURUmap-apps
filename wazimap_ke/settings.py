02ef634945114e2a622c4596fcf6ba7112e8234e
Greg Kempe <greg@kempe.net>
2016-02-09 15:26:32 +0200

    Override profile details.

--- wazimap_ke/settings.py
+++ wazimap_ke/settings.py
@@ -1,6 +1,16 @@
+import os
+
 # pull in the default wazimap settings
 from wazimap.settings import *  # noqa
 
 
+# insert our overrides before both census and wazimap
+INSTALLED_APPS.insert(-2, 'wazimap_ke')
+
+
+DATABASE_URL = os.environ.get('DATABASE_URL', 'postgresql://censusreporter_ke:censusreporter_ke@localhost/censusreporter_ke')
+
+
 # Localise this instance of Wazimap
 WAZIMAP['name'] = 'Wazimap Kenya'
+WAZIMAP['profile_builder'] = 'wazimap_ke.profiles.get_census_profile'
