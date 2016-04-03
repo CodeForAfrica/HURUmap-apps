a02f4af8a26fe02a6ed85b3115f5ba07a400c47c
David Lemayian <david@on.co.ke>
2016-04-03 12:08:00 +0300

    Added about content on homepage.

--- wazimap_ke/settings.py
+++ wazimap_ke/settings.py
@@ -16,7 +16,7 @@ DATABASES['default']['ATOMIC_REQUESTS'] = True
 
 
 # Localise this instance of Wazimap
-WAZIMAP['name'] = 'Wazimap Kenya'
+WAZIMAP['name'] = 'WAZImap Kenya'
 WAZIMAP['url'] = 'http://kenya.wazimap.org'
 WAZIMAP['country_code'] = 'KE'
 WAZIMAP['profile_builder'] = 'wazimap_ke.profiles.get_census_profile'
