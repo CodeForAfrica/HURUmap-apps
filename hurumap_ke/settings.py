94750ab1751826c6f65893262abce3ba1c878b7b
David Lemayian <dlemayian@gmail.com>
2017-08-06 18:24:05 +0300

    An example of combined code instead of multiple branches.
    Installed at will using environment variables. Some things:
    
    1. Set `DJANGO_SETTINGS_MODULE` to the settings you’d like.
    2. Run `python manage.py collectstatic`
    
    Some pitfalls foreseen:
    1. CHECKS will be hard to write. Need to check or statements.
    2. We need to update the README.md thoroughly.

--- hurumap_ke/settings.py
+++ hurumap_ke/settings.py
@@ -37,7 +37,7 @@ HURUMAP['geometry_data'] = {
     'county': 'geo/county.topojson',
 }
 
-HURUMAP['ga_tracking_id'] = 'UA-44795600-8'
+HURUMAP['ga_tracking_id'] = ['UA-44795600-8']
 HURUMAP['twitter'] = '@Code4Africa'
 
 HURUMAP['map_centre'] = [0.3051933453207569, 37.908818734483155]
