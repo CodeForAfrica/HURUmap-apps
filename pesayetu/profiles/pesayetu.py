005a1350d2cfdebc10878e0badf15e73e0495f53
Phillip Ahereza <pahereza@gmail.com>
2017-11-21 12:41:47 +0300

    profile page displaying equitable allocation data

--- pesayetu/profiles/pesayetu.py
+++ pesayetu/profiles/pesayetu.py
@@ -4,7 +4,6 @@ from wazimap.data.utils import (LocationNotFound, get_session, get_stat_data,
 from django.conf import settings
 import logging
 
-
 log = logging.getLogger(__name__)
 
 SECTIONS = settings.HURUMAP.get('topics', {})
@@ -27,12 +26,56 @@ def get_profile(geo, profile_name, request):
 def get_equitable_allocation_data(geo, session):
     try:
         budget_allocation, _ = get_stat_data('financial_year', geo, session,
-                                                table_fields=[
-                                                    'financial_year'], percent=False)
-        print type(budget_allocation)
-        return {
-            'budget_allocation': budget_allocation
+                                             table_fields=[
+                                                 'financial_year'],
+                                             percent=False)
+
+        yr2013_2014 = 0
+        print budget_allocation
+        for data, value in budget_allocation.get('FY 2013/2014', {}).iteritems():
+            if data == 'values':
+                yr2013_2014 += value['this']
+
+        yr2014_2015 = 0
+        for data, value in budget_allocation.get('FY 2014/2015', {}).iteritems():
+            if data == 'values':
+                yr2014_2015 += value['this']
+
+        yr2015_2016 = 0
+        for data , value in budget_allocation.get('FY 2015/2016', {}).iteritems():
+            if data == 'values':
+                yr2015_2016 += value['this']
+
+        yr2016_2017 = 0
+        for data, value in budget_allocation.get('FY 2016/2017', {}).iteritems():
+            if data == 'values':
+                yr2016_2017 += value['this']
+
+        final_data = {
+            'budget_allocation': budget_allocation,
+            'fy2013_2014': {
+                'name': 'FY 2013/2014',
+                'numerators': {'this': yr2013_2014},
+                'values': {'this': yr2013_2014}
+            },
+            'fy2014_2015': {
+                'name': 'FY 2014/2015',
+                'numerators': {'this': yr2014_2015},
+                'values': {'this': yr2014_2015}
+            },
+            'fy2015_2016': {
+                'name': 'FY 2015/2016',
+                'numerators': {'this': yr2015_2016},
+                'values': {'this': yr2015_2016}
+            },
+            'fy2016_2017': {
+                'name': 'FY 2016/2017',
+                'numerators': {'this': yr2016_2017},
+                'values': {'this': yr2016_2017}
+            },
         }
+
+        return final_data
 
     except LocationNotFound:
         budget_allocation, _ = LOCATIONNOTFOUND, 0
