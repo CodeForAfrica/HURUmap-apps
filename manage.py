c86dd349416d49b1935fc5ca2fe9a1061db5515f
Timothy Anthony <anthony.timothy90@gmail.com>
2017-12-11 09:30:08 +0300

    Changing Django module settings to hurumap

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "league_table.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hurumap.settings")
 
     from django.core.management import execute_from_command_line
 
