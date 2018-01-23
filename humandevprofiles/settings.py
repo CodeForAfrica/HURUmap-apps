8d8597ef4c235218283139b4256853a6c48745c9
Phillip Ahereza <pahereza@gmail.com>
2018-01-23 10:44:08 +0300

    added showcase stories

--- humandevprofiles/settings.py
+++ humandevprofiles/settings.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from hurumap.settings import *  # noqa
 
 # insert our overrides before both census and hurumap
@@ -10,23 +11,48 @@ DATABASES['default'] = dj_database_url.parse(DATABASE_URL)
 DATABASES['default']['ATOMIC_REQUESTS'] = True
 
 
-HUMANDEV = WAZIMAP
+HURUMAP = WAZIMAP
 
-HUMANDEV['name'] = 'Human Development Profiles'
-HUMANDEV['url'] = 'https://hd.hurumap.org'
+HURUMAP['name'] = 'Human Development Profiles'
+HURUMAP['url'] = 'https://hd.hurumap.org'
 
 humandev_profile = os.environ.get('HUMANDEV_PROFILE', 'hd_profiles')
 
 
-HUMANDEV['default_profile'] = humandev_profile
+HURUMAP['default_profile'] = humandev_profile
 
-HUMANDEV['profile_builder'] = 'humandevprofiles.profiles.{}.get_profile'.format(
+HURUMAP['profile_builder'] = 'humandevprofiles.profiles.{}.get_profile'.format(
     humandev_profile)
-HUMANDEV['default_geo_version'] = os.environ.get('DEFAULT_GEO_VERSION', '2009')
-HUMANDEV['legacy_embed_geo_version'] = '2009'
+HURUMAP['default_geo_version'] = os.environ.get('DEFAULT_GEO_VERSION', '2009')
+HURUMAP['legacy_embed_geo_version'] = '2009'
+
+HURUMAP['showcase_stories'] = [
+    {
+        'title': 'Figures of the week: Human development progress in Africa and globally',
+        'author': 'Amy Copley',
+        'brief': 'Figures of the week: Human development progress in Africa and globally',
+        'link': 'https://www.brookings.edu/blog/africa-in-focus/2017/03/24/figures-of-the-week-human-development-progress-in-africa-and-globally/',
+        'img': STATIC_URL + 'img/showcase/africa_hdi.jpg'
+    },
+    {
+        'title': "Analysis: South Africa’s Performance in UNDP Human "
+                 "Development Report 2015",
+        'author': 'Nancy Agutu',
+        'brief': "Analysis: South Africa’s Performance in UNDP Human Development Report 2015.",
+        'link': 'https://www.brandsouthafrica.com/south-africa-fast-facts/news-facts/analysis-south-africa-s-performance-in-undp-human-development-report-2015',
+        'img': STATIC_URL + 'img/showcase/southafrica_analysis.gif'
+    },
+    {
+        'title': 'Malawi\'s human development index on the rise-UN report',
+        'author': 'Monicah Mwangi',
+        'brief': 'Malawi\'s human development index on the rise-UN report',
+        'link': 'http://www.africanews.com/2017/03/23/malawi-s-human-development-index-on-the-rise-un-report/',
+        'img': STATIC_URL + 'img/showcase/malawi_hdi.jpg'
+    }
+]
 
 
-HUMANDEV['levels'] = {
+HURUMAP['levels'] = {
     'continent': {
         'plural': 'continents',
         'children': ['country'],
@@ -35,8 +61,8 @@ HUMANDEV['levels'] = {
         'plural': 'countries',
     }
 }
-HUMANDEV['comparative_levels'] = ['country']
-HUMANDEV['geometry_data'] = {
+HURUMAP['comparative_levels'] = ['country']
+HURUMAP['geometry_data'] = {
     '2009': {
         'continent': 'geo/continent.topojson',
         'country': 'geo/country.topojson'
@@ -46,4 +72,4 @@ HUMANDEV['geometry_data'] = {
 LOGGING['loggers']['humandev'] = {'level': 'DEBUG' if DEBUG else 'INFO'}
 
 # Making sure they are the same
-WAZIMAP = HUMANDEV
+WAZIMAP = HURUMAP
