42216f2100488dab3b84b9848c194f191d728285
nerdlet <brndnyokabi@gamil.com>
2018-04-10 15:28:01 +0300

    Update template

--- wajibisha/urls.py
+++ wajibisha/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 from wazimap.urls import *
 from .views import ArticlesView
+from views import AboutView
 
 urlpatterns += [
     url(r'^articles/$', ArticlesView.as_view(), name='articles'),
+    url(r'^about/$', AboutView.as_view(), name='about')
 ]
 
 
--- wajibisha/views.py
+++ wajibisha/views.py
@@ -3,3 +3,5 @@ from django.views.generic import TemplateView
 
 class ArticlesView(TemplateView):
     template_name = 'articles.html'
+class AboutView(TemplateView):
+	template_name='about.html'
