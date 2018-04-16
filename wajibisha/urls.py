aad5148cae48f0adc3eb2ba179fdc073ba3050bd
nerdlet <brndnyokabi@gamil.com>
2018-04-16 14:49:17 +0300

    Update changes on templates

--- wajibisha/urls.py
+++ wajibisha/urls.py
@@ -2,10 +2,12 @@ from django.conf.urls import url
 from wazimap.urls import *
 from .views import ArticlesView
 from views import AboutView
+from views import PromisesView
 
 urlpatterns += [
     url(r'^articles/$', ArticlesView.as_view(), name='articles'),
-    url(r'^about/$', AboutView.as_view(), name='about')
+    url(r'^about/$', AboutView.as_view(), name='about'),
+    url(r'^promises/$', PromisesView.as_view(), name='promises')
 ]
 
 
--- wajibisha/views.py
+++ wajibisha/views.py
@@ -4,4 +4,7 @@ from django.views.generic import TemplateView
 class ArticlesView(TemplateView):
     template_name = 'articles.html'
 class AboutView(TemplateView):
-	template_name='about.html'
+	template_name = 'about.html'
+class PromisesView(TemplateView):
+	template_name = 'promises.html'
+
