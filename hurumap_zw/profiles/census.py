1597b60edf1da9673c91045381de4eed0d5e22f2
Phillip Ahereza <pahereza@gmail.com>
2018-06-12 10:22:09 +0300

    added profile

---
+++ hurumap_zw/profiles/census.py
@@ -0,0 +1,11 @@
+from wazimap.data.utils import get_session
+
+def get_profile(geo, profile_name, request):
+    session = get_session()
+    data = {}
+
+    try:
+
+        return data
+    finally:
+        session.close()
