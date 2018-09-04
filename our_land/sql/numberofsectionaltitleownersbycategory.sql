a1796cdde3e71aeecc7edbfba98ed01a0e265f41
KhadijaMahanga <khadijamahanga@y7mail.com>
2018-09-04 11:13:05 +0300

    changes in sql

--- our_land/sql/numberofsectionaltitleownersbycategory.sql
+++ our_land/sql/numberofsectionaltitleownersbycategory.sql
@@ -7,13 +7,13 @@
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
+
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SELECT pg_catalog.set_config('search_path', '', false);
 SET check_function_bodies = false;
 SET client_min_messages = warning;
-SET row_security = off;
+
 
 ALTER TABLE IF EXISTS ONLY public.numberofsectionaltitleownersbycategory DROP CONSTRAINT IF EXISTS pk_numberofsectionaltitleownersbycategory;
 DROP TABLE IF EXISTS public.numberofsectionaltitleownersbycategory;
