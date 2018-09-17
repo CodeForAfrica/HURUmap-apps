908028cfd77ff87dc7d445665f8e51f86f67c60e
_ Kilemensi <kilemensi@gmail.com>
2018-09-17 13:34:02 +0300

    Migrate to HURUmap@master

--- elimu_yangu/careerguide/urls.py
+++ elimu_yangu/careerguide/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls import url, patterns, include
+from django.conf.urls import url, include
 from elimu_yangu.careerguide.views import school, alevel_subjects, olevel_subjects, index
 from django.conf.urls.i18n import i18n_patterns
 #from wazimap.urls import *
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(regex = r'^$', view = index, name='index'),
-    url(regex = 'school/(?P<schoolcode>[\w.-]+)/$', view = school, name='school'),
-    url(regex = 'subjects/a-level', view = alevel_subjects, name='subjects/a-level'),
-    url(regex = 'subjects/o-level', view = olevel_subjects, name='subjects/o-level'),
-    )
+    url(regex = r'school/(?P<schoolcode>[\w.-]+)/$', view = school, name='school'),
+    url(regex = r'subjects/a-level', view = alevel_subjects, name='subjects/a-level'),
+    url(regex = r'subjects/o-level', view = olevel_subjects, name='subjects/o-level'),
+]
--- elimu_yangu/urls.py
+++ elimu_yangu/urls.py
@@ -1,5 +1,5 @@
 from hurumap.urls import urlpatterns as hurumap_urlpatterns
-from django.conf.urls import include, patterns, url
+from django.conf.urls import include, url
 from django.conf.urls.i18n import i18n_patterns
 from elimu_yangu.views import HomepageView, AboutView
 
@@ -9,22 +9,19 @@ urlpatterns = i18n_patterns(
     url(r'^careerguide/', include('elimu_yangu.careerguide.urls')),
     #prefix_default_language = False,
     url(
-        regex   = '^$',
+        regex   = r'^$',
         view    = HomepageView.as_view(),
         kwargs  = {},
         name    = 'homepage',
     ),
     url(
-        regex   = '^about$',
+        regex   = r'^about$',
         view    = AboutView.as_view(),
         kwargs  = {},
         name    = 'about',
     ),
-     #prefix_default_language=False,
-    # url(r'^i18n/', include('django.conf.urls.i18n')),,
-
 ) + hurumap_urlpatterns
 
-urlpatterns += patterns('',
-     url(r'^i18n/', include('django.conf.urls.i18n'))
- )
+urlpatterns += [
+    url(r'^i18n/', include('django.conf.urls.i18n'))
+]
