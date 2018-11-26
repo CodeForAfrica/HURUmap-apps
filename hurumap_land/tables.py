b9b32d489e707d9ff25453d8be31f79a0a150ee7
Phillip Ahereza <pahereza@gmail.com>
2018-11-26 13:02:33 +0300

    added afrobarometer land survey

--- hurumap_land/tables.py
+++ hurumap_land/tables.py
@@ -153,3 +153,30 @@ SimpleTable(
     dataset='Land Redistribution and Restitution Statistic 2018',
     year='2016'
 )
+
+
+
+FieldTable(['access_to_information'], universe='Land in South Africa',
+           year='2017', dataset='Afrobarometer Survey')
+
+FieldTable(['allow_farmers_retain_land_ownership'],
+           universe='Land in South Africa', year='2017',
+           dataset='Afrobarometer Survey')
+
+FieldTable(['maintain_willing_buyer_willing_seller_policy'],
+           universe='Land in South Africa', year='2017',
+           dataset='Afrobarometer Survey')
+
+FieldTable(['land_acquisation_challenges'], universe='Land in South Africa',
+           year='2017', dataset='Afrobarometer Survey')
+
+FieldTable(['land_to_prioritise_for_redistribution'],
+           universe='Land in South Africa', year='2017',
+           dataset='Afrobarometer Survey')
+
+FieldTable(['women_have_equal_right_to_land'], universe='Land in South Africa',
+           year='2017', dataset='Afrobarometer Survey')
+
+FieldTable(['women_men_equal_chance_own_land'], universe='Land in South Africa',
+           year='2017', dataset='Afrobarometer Survey')
+
