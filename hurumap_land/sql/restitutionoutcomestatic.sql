e10bf8428a53ffc48011453c4c1bad9f5fc36c23
KhadijaMahanga <khadijamahanga@y7mail.com>
2018-12-13 12:43:19 +0300

    re-add restitution missing data + other sql edits

--- hurumap_land/sql/restitutionoutcomestatic.sql
+++ hurumap_land/sql/restitutionoutcomestatic.sql
@@ -7,7 +7,6 @@
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SELECT pg_catalog.set_config('search_path', '', false);
@@ -21,7 +20,7 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
--- Name: restitutionoutcomestatistic; Type: TABLE; Schema: public; Owner: our_land
+-- Name: restitutionoutcomestatistic; Type: TABLE; Schema: public; 
 --
 
 CREATE TABLE public.restitutionoutcomestatistic (
@@ -37,7 +36,7 @@ CREATE TABLE public.restitutionoutcomestatistic (
 ALTER TABLE IF EXISTS public.restitutionoutcomestatistic OWNER TO our_land;
 
 --
--- Data for Name: restitutionoutcomestatistic; Type: TABLE DATA; Schema: public; Owner: our_land
+-- Data for Name: restitutionoutcomestatistic; Type: TABLE DATA; Schema: public;
 --
 
 COPY public.restitutionoutcomestatistic (geo_level, geo_code, geo_version, restitutionoutcomestatistic, year, total) FROM stdin;
