2a8e2e82f732a631b74c12d47e54491c30cd380c
KhadijaMahanga <khadijamahanga@y7mail.com>
2019-01-14 10:46:07 +0300

    remove router and api path

--- dominion/urls.py
+++ dominion/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import include, url
-from django.urls import path
 from django.conf.urls.static import static
 from django.views.generic import RedirectView
 
-from rest_framework import routers
+#from rest_framework import routers
+# #[url(r'^api/', include(router.urls))] + \
 
 from dominion import settings
 from hurumap.urls import urlpatterns as hurumap_urlpatterns
@@ -11,5 +11,4 @@ from hurumap.urls import urlpatterns as hurumap_urlpatterns
 
 urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + \
               static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + \
-              [path('api/', include(router.urls))] + \
               hurumap_urlpatterns
