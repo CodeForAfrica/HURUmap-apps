816cfcc80c05e2c6d94e75af53317e380a6046c1
KhadijaMahanga <khadijamahanga@y7mail.com>
2019-01-15 11:47:36 +0300

    edit path for css

--- dominion-frontend/src/App.js
+++ dominion-frontend/src/App.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
 
-import CssBaseline from '@material-ui/core/CssBaseline';
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-
 import logo from './logo.svg';
-import './App.css';
+import './assets/css/App.css';
 
 class App extends Component {
   render() {
