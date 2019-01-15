984cb9e0c5fcf7e5ddaec89c6d771080f70501a3
KhadijaMahanga <khadijamahanga@y7mail.com>
2019-01-15 11:46:07 +0300

    remove rest_framework import

--- dominion/urls.py
+++ dominion/urls.py
@@ -2,9 +2,6 @@ from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.views.generic import RedirectView
 
-#from rest_framework import routers
-# #[url(r'^api/', include(router.urls))] + \
-
 from dominion import settings
 from hurumap.urls import urlpatterns as hurumap_urlpatterns
 
