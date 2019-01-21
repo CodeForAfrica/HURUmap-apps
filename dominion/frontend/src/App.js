810d79d094673e6c1bad4682db87a07fd326ac31
nerdlet <brndnyokabi@gmail.com>
2019-01-21 14:50:15 +0300

    Minor fixes

--- dominion/frontend/src/App.js
+++ dominion/frontend/src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import CssBaseline from "@material-ui/core/CssBaseline";
 
 import Home from "./pages/home";
-import { Typography } from "@material-ui/core";
+//import { Typography } from "@material-ui/core";
 
 class App extends Component {
   render() {
