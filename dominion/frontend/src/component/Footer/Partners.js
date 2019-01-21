35ed1fd2d4cef08c1c44fb903fd20907bd0f0dec
nerdlet <brndnyokabi@gmail.com>
2019-01-21 11:30:50 +0300

    Inital component

---
+++ dominion/frontend/src/component/Footer/Partners.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Grid, Typography } from "@material-ui/core";
+
+function Partners() {
+  return (
+    <Grid>
+      <Typography variant="title">Partner's Logos</Typography>
+    </Grid>
+  );
+}
+
+export default Partners;
---
+++ dominion/frontend/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Footer from "../component/Footer/Index";
+
+function Home() {
+  return (
+    <React.Fragment>
+      <Footer />
+    </React.Fragment>
+  );
+}
+
+export default Home;
