19f730fd9e0a7f3cc3284e44f48baa86c85ae69d
nerdlet <brndnyokabi@gmail.com>
2019-01-21 12:36:03 +0300

    Configure default routes and favicons

---
+++ dominion/frontend/src/component/DocumentHead/PageHeads.js
@@ -0,0 +1,12 @@
+const URLS = {
+  HOME: "/"
+};
+export { URLS };
+
+export default [
+  {
+    url: URLS.HOME,
+    title: "Dominion | Home",
+    color: "darkgrey"
+  }
+];
--- dominion/frontend/src/pages/Home.js
+++ dominion/frontend/src/pages/Home.js
@@ -1,9 +1,12 @@
 import React from "react";
+
 import Footer from "../component/Footer/Index";
+import DocumentHead from "../component/DocumentHead/Index";
 
 function Home() {
   return (
     <React.Fragment>
+      <DocumentHead />
       <Footer />
     </React.Fragment>
   );
