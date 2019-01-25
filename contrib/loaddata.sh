0c32a70ff3621c991e9d12356cf01ed972421f10
Phillip Ahereza <pahereza@gmail.com>
2019-01-25 07:41:53 +0300

    add universityfinder fixtures

--- contrib/loaddata.sh
+++ contrib/loaddata.sh
@@ -4,3 +4,9 @@ if [ -z ${HURUMAP_APP+x} ]; then HURUMAP_APP='hurumap_land'; fi
 
 cat ${HURUMAP_APP}/sql/*.sql | psql            # Load tables / data into DB (TBD)
 python manage.py loaddata ${HURUMAP_APP}/fixtures/${HURUMAP_APP}.json  # Load fixtures
+
+# load universityfinder data if elimu_yangu
+if [${HURUMAP_APP} == "elimu_yangu"]; then
+    python manage.py migrate # migrate the elimu yangu tables
+    python manage.py loaddata elimu_yangu/fixtures/universityfinder.json;
+fi
