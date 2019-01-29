ed0783fa8213bcf9255f4ab097e171b685e50a4d
nerdlet <brndnyokabi@gmail.com>
2019-01-29 12:36:15 +0300

    Add scroll to top

--- dominion/frontend/src/App.js
+++ dominion/frontend/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
+import ScrollToTop from './ScrollToTop';
 import { URLS } from './component/DocumentHead/PageHeads';
 import Home from './pages/Home';
 import Country from './pages/Country';
@@ -13,8 +14,8 @@ const FONT_FAMILY = '"Roboto", "sans-serif"';
 
 const THEME = createMuiTheme({
   palette: {
-    primary: { main: '#ffffff', light: '#f1f1ed', dark: '#1b1b1a' },
-    secondary: { main: '#000000', light: '#2c2c2a', dark: '#2b3129' },
+    primary: { main: '#ffffff', light: '#f1f1ed' },
+    secondary: { main: '#000000', dark: '#2c2c2a', grey: '#2b3129' },
     highlight: { main: '#e7e452' }
   },
   typography: {
@@ -42,20 +43,22 @@ function App() {
     <MuiThemeProvider theme={THEME}>
       <CssBaseline>
         <BrowserRouter>
-          <Switch>
-            <Route
-              exact
-              path={URLS.HOME}
-              render={props => <Home {...props} url={URLS.HOME} />}
-            />
-            <Route
-              path={URLS.COUNTRY}
-              render={props => <Country {...props} url={URLS.COUNTRY} />}
-            />
-            <Route
-              render={props => <NotFound {...props} url={URLS.NOT_FOUND} />}
-            />
-          </Switch>
+          <ScrollToTop>
+            <Switch>
+              <Route
+                exact
+                path={URLS.HOME}
+                render={props => <Home {...props} url={URLS.HOME} />}
+              />
+              <Route
+                path={URLS.COUNTRY}
+                render={props => <Country {...props} url={URLS.COUNTRY} />}
+              />
+              <Route
+                render={props => <NotFound {...props} url={URLS.NOT_FOUND} />}
+              />
+            </Switch>
+          </ScrollToTop>
         </BrowserRouter>
       </CssBaseline>
     </MuiThemeProvider>
