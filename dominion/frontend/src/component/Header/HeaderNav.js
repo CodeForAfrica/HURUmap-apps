d594e5b54ab810399c3335f90e60b35d0b839419
nerdlet <brndnyokabi@gmail.com>
2019-02-04 15:48:38 +0300

    Include toggle icon

--- dominion/frontend/src/component/Header/HeaderNav.js
+++ dominion/frontend/src/component/Header/HeaderNav.js
@@ -1,29 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-import {
-  Grid,
-  Toolbar,
-  Typography,
-  MenuList,
-  MenuItem
-} from '@material-ui/core';
-import { withStyles } from '@material-ui/core/styles';
-
+import MenuOutlined from '@material-ui/icons/MenuOutlined';
 import SearchOutlined from '@material-ui/icons/SearchOutlined';
 import KeyboardArrowDownOutlined from '@material-ui/icons/KeyboardArrowDownOutlined';
 
 import logo from '../../assets/images/logos/dominion-logo.png';
 
+import {
+  Grid,
+  Typography,
+  MenuList,
+  MenuItem,
+  Toolbar
+} from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+
+import '../../assets/App.css';
+
 const styles = theme => ({
   root: {
     flexGrow: 1
-  },
-  toolbar: {
-    margin: '4rem'
-  },
-  appbar: {
-    backgroundColor: 'transparent',
-    border: 0
   },
   img: {
     maxWidth: '100%',
@@ -47,90 +44,139 @@ const styles = theme => ({
   icon: {
     color: 'white',
     fontSize: 30
+  },
+  togglebreakpoint: {
+    color: 'white',
+    [theme.breakpoints.up('xs')]: {
+      color: 'red'
+    }
   }
 });
-function HeaderNav({ classes }) {
-  return (
-    <nav postion="static" className={classes.appbar}>
-      <Toolbar className={classes.toolbar}>
-        <Grid item xs={4} className={classes.text}>
-          <img src={logo} alt="Dominion Logo" className={classes.img} />
-        </Grid>
 
-        <Grid container direction="row" justify="center" alignItems="center">
-          <MenuList
-            item
-            className={classes.text}
-            direction="row"
-            justify="space-evenly"
-            alignItems="center"
-            style={{ display: 'flex' }}
-          >
-            <MenuItem item>
-              <a href="/" className={classes.links}>
-                <Typography variant="body1" className={classes.text}>
-                  About
-                </Typography>
-              </a>
-            </MenuItem>
+class HeaderNav extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menu_class: ''
+    };
+  }
 
-            <MenuItem item>
-              <a href="/" className={classes.links}>
-                <Typography variant="body1" className={classes.text}>
-                  Showcase
-                </Typography>
-              </a>
-            </MenuItem>
+  setToggleTopMenuClass = () => {
+    if (this.state.menu_class === '') {
+      this.setState({
+        menu_class: 'toggled'
+      });
+    } else {
+      this.setState({
+        menu_class: ''
+      });
+    }
+  };
 
-            <MenuItem item>
-              <a href="/" className={classes.links}>
-                <Typography variant="body1" className={classes.text}>
-                  Resources
-                </Typography>
-              </a>
-            </MenuItem>
-
-            <MenuItem item>
-              <a href="/" className={classes.links}>
-                <Typography variant="body1" className={classes.text}>
-                  {' '}
-                  Contact
-                </Typography>
-              </a>
-            </MenuItem>
-          </MenuList>
+  render = () => {
+    const { classes } = this.props;
+    const top_menu_class = `top-menu ${this.state.menu_class}`;
+    return (
+      <nav position="static" className={top_menu_class}>
+        <Toolbar>
+          <Grid item xs={4} className="top-menu-lead">
+            <img src={logo} alt="Dominion Logo" className={classes.img} />
+          </Grid>
 
           <Grid
             container
-            xs={12}
-            lg={6}
             direction="row"
-            spacing={40}
-            justify="space-evenly"
+            justify="center"
             alignItems="center"
+            className="menu-nav"
           >
-            <Grid item>
-              <Grid
-                container
-                direction="row"
-                justify="space-between"
-                alignItems="center"
-              >
-                <Typography variant="body1" className={classes.text}>
-                  Countries
-                </Typography>
-                <KeyboardArrowDownOutlined className={classes.icon} />
+            <MenuList
+              item
+              className={classes.text}
+              direction="row"
+              justify="space-evenly"
+              alignItems="center"
+              style={{ display: 'flex' }}
+            >
+              <MenuItem item>
+                <a href="/" className={classes.links}>
+                  <Typography variant="body1" className={classes.text}>
+                    About
+                  </Typography>
+                </a>
+              </MenuItem>
+
+              <MenuItem item>
+                <a href="/" className={classes.links}>
+                  <Typography variant="body1" className={classes.text}>
+                    Showcase
+                  </Typography>
+                </a>
+              </MenuItem>
+
+              <MenuItem item>
+                <a href="/" className={classes.links}>
+                  <Typography variant="body1" className={classes.text}>
+                    Resources
+                  </Typography>
+                </a>
+              </MenuItem>
+
+              <MenuItem item>
+                <a href="/" className={classes.links}>
+                  <Typography variant="body1" className={classes.text}>
+                    {' '}
+                    Contact
+                  </Typography>
+                </a>
+              </MenuItem>
+            </MenuList>
+
+            <Grid
+              container
+              xs={12}
+              lg={6}
+              direction="row"
+              spacing={40}
+              justify="space-evenly"
+              alignItems="center"
+            >
+              <Grid item>
+                <Grid
+                  container
+                  direction="row"
+                  justify="space-between"
+                  alignItems="center"
+                >
+                  <Typography variant="body1" className={classes.text}>
+                    Countries
+                  </Typography>
+                  <KeyboardArrowDownOutlined className={classes.icon} />
+                </Grid>
+              </Grid>
+
+              <Grid item className={classes.text}>
+                <SearchOutlined />
               </Grid>
             </Grid>
-
-            <Grid item className={classes.text}>
-              <SearchOutlined />
-            </Grid>
           </Grid>
-        </Grid>
-      </Toolbar>
-    </nav>
-  );
+
+          <Grid>
+            <MenuOutlined
+              color="primary"
+              className="top-menu-icon"
+              onClick={this.setToggleTopMenuClass}
+            />
+            <div className="clear-fix" />
+          </Grid>
+        </Toolbar>
+      </nav>
+    );
+  };
 }
+
+HeaderNav.propTypes = {
+  classes: PropTypes.object.isRequired
+};
 
 export default withStyles(styles)(HeaderNav);
