d174bbce5b0fff9e6f6c20597207da1a677bedc8
_ Kilemensi <kilemensi@gmail.com>
2019-02-06 15:39:55 +0300

    Initial implementation of Showcase component

---
+++ dominion/frontend/src/component/Showcase/Stories.js
@@ -0,0 +1,58 @@
+import background from '../../assets/images/Showcase/background.png';
+
+const data = [
+  {
+    index: 0,
+    image: background,
+    date: '12 Nov 2019',
+    title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    body:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur.',
+    link: 'https://codeforafrica.org'
+  },
+  {
+    index: 1,
+    image: background,
+    date: '12 Nov 2019',
+    title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    body:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur.',
+    link: 'https://codeforafrica.org'
+  },
+  {
+    index: 2,
+    image: background,
+    date: '12 Nov 2019',
+    title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    body: '',
+    link: 'https://codeforafrica.org'
+  },
+  {
+    index: 3,
+    image: background,
+    date: '12 Nov 2019',
+    title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    body: '',
+    link: 'https://codeforafrica.org'
+  },
+  {
+    index: 4,
+    image: background,
+    date: '12 Nov 2019',
+    title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    body:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur.',
+    link: 'https://codeforafrica.org'
+  },
+  {
+    index: 5,
+    image: background,
+    date: '12 Nov 2019',
+    title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    body:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur.',
+    link: 'https://codeforafrica.org'
+  }
+];
+
+export default data;
--- dominion/frontend/src/pages/Home.js
+++ dominion/frontend/src/pages/Home.js
@@ -4,11 +4,13 @@ import PropTypes from 'prop-types';
 import DocumentHead from '../component/DocumentHead';
 import Footer from '../component/Footer';
 import Partners from '../component/Footer/Partners';
+import Showcase from '../component/Showcase';
 
 function Home({ url }) {
   return (
     <React.Fragment>
       <DocumentHead url={url} />
+      <Showcase />
       <Partners />
       <Footer />
     </React.Fragment>
