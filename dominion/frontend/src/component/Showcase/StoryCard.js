0e0b0f1f6b1a8526ce87cbfd39895897c52c43a8
_ Kilemensi <kilemensi@users.noreply.github.com>
2019-02-07 09:46:21 +0300

    Revert "Revert "[Dominion] [Site] Landing Page Showcase Component""

---
+++ dominion/frontend/src/component/Showcase/StoryCard.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import {
+  Typography,
+  Card,
+  CardActionArea,
+  CardMedia,
+  CardContent
+} from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+  root: {
+    width: '100vw',
+    height: '100%',
+    backgroundColor: '#fafafa',
+    border: '1px solid #eeeeee',
+    opacity: 0.9,
+    '&:hover': {
+      opacity: 1,
+      backgroundColor: '#fff'
+    },
+    [theme.breakpoints.up('md')]: {
+      width: '14.90625rem'
+    },
+    [theme.breakpoints.up('lg')]: {
+      width: '19.875rem'
+    }
+  },
+  media: {
+    height: 0,
+    paddingTop: '100%',
+    width: '100%'
+  },
+  cardLink: {
+    textDecoration: 'none'
+  },
+  overline: {
+    color: '#fff',
+    fontSize: '14px',
+    paddingTop: '1rem'
+  },
+  bodyArea: {
+    paddingTop: '2rem'
+  },
+  bodyTitle: {
+    color: '#fff',
+    fontWeight: 400
+  },
+  bodyText: {
+    color: '#fff',
+    fontWeight: 400
+  }
+});
+
+function StoryCard({ story, classes }) {
+  const { image, date, title, body, link } = story;
+
+  return (
+    <Card className={classes.root}>
+      <a
+        href={link}
+        target="_blank"
+        rel="noopener noreferrer"
+        className={classes.cardLink}
+      >
+        <CardActionArea
+          style={{ display: 'flex', flexFlow: 'column', height: '100%' }}
+        >
+          <CardMedia className={classes.media} image={image} title=" Story" />
+          <CardContent style={{ flexGrow: 1, marginTop: '-100%' }}>
+            <Typography variant="h5" className={classes.overline}>
+              {date}
+            </Typography>
+            <div className={classes.bodyArea}>
+              <Typography variant="h5" className={classes.bodyTitle}>
+                {title}
+              </Typography>
+              <Typography variant="subtitle2" className={classes.bodyText}>
+                {body}{' '}
+              </Typography>
+            </div>
+          </CardContent>
+        </CardActionArea>
+      </a>
+    </Card>
+  );
+}
+
+StoryCard.propTypes = {
+  classes: PropTypes.shape().isRequired,
+  story: PropTypes.shape().isRequired
+};
+
+export default withStyles(styles)(StoryCard);
--- dominion/frontend/src/pages/Home.js
+++ dominion/frontend/src/pages/Home.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Footer from '../component/Footer';
+
 import DocumentHead from '../component/DocumentHead';
+import Footer from '../component/Footer';
+import Showcase from '../component/Showcase';
 
 function Home({ url }) {
   return (
     <React.Fragment>
       <DocumentHead url={url} />
+      <Showcase />
       <Footer />
     </React.Fragment>
   );
