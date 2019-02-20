2ad3c2d299cb6fcac795b3ad3d66a805719f424f
Phillip Ahereza <pahereza@gmail.com>
2019-02-20 09:56:06 +0300

    moved google geocode api to env

--- hurumap_global/settings.py
+++ hurumap_global/settings.py
@@ -2,7 +2,9 @@ from hurumap.settings import *  # noqa
 
 INSTALLED_APPS = ['hurumap_global'] + INSTALLED_APPS
 
+HURUMAP['google_geocode_api_key'] = os.environ.get('GOOGLE_GEOCODE_API_KEY')
+
 MIDDLEWARE_CLASSES = (
         'whitenoise.middleware.WhiteNoiseMiddleware',
     ) + MIDDLEWARE_CLASSES
-    
+WAZIMAP = HURUMAP
