0137aa500ebb9a6ad27adf997a39bdab5885b22c
_ Kilemensi <kilemensi@gmail.com>
2019-02-22 10:36:56 +0300

    Update hurumap image version & format code

--- hurumap_global/settings.py
+++ hurumap_global/settings.py
@@ -5,6 +5,7 @@ INSTALLED_APPS = ['hurumap_global'] + INSTALLED_APPS
 HURUMAP['google_geocode_api_key'] = os.environ.get('GOOGLE_GEOCODE_API_KEY')
 
 MIDDLEWARE_CLASSES = (
-        'whitenoise.middleware.WhiteNoiseMiddleware',
-    ) + MIDDLEWARE_CLASSES
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+) + MIDDLEWARE_CLASSES
+
 WAZIMAP = HURUMAP
