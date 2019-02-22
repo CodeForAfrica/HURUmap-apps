0ff219d940be25bddf2ec2f8148d8b8e74ffb87b
KhadijaMahanga <khadijamahanga@y7mail.com>
2019-02-22 15:08:54 +0300

    import static on urls file

--- elimu_yangu/urls.py
+++ elimu_yangu/urls.py
@@ -1,7 +1,10 @@
 from hurumap.urls import urlpatterns as hurumap_urlpatterns
+from django.conf.urls.static import static
 from django.conf.urls import include, url
 from django.conf.urls.i18n import i18n_patterns
 from elimu_yangu.views import HomepageView, AboutView, ProfileRedirectView
+
+from elimu_yangu import settings
 
 urlpatterns = i18n_patterns(
     url(
@@ -31,4 +34,4 @@ urlpatterns = i18n_patterns(
 urlpatterns += [
     url(r'^i18n/', include('django.conf.urls.i18n'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + \
-    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
+    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
