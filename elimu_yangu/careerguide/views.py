76fadb41a3f5de34655c627d97009aa7e5d99fd4
Khadija Mahanga <khadijamahanga@y7mail.com>
2019-02-25 02:02:15 -0800

    [ElimuYangu] Bug Fixes (#136)
    * fix place search and comparison
    
    * remove django session on middleware
    
    * add whitenoise and static
    
    * replace cft logo with cfa logo
    
    * rename lib to libs
    
    * import static on urls file
    
    * added template context processor and organised middleware
    
    * remove unused import
    
    * remove get_current_language as sw
    
    * fix whitenoise error, remove unfound file
    
    * fix whitenoise error, remove unfound file
    
    * remove ward
    
    * Update site js. Replace append with html div
    
    * edit template settings to new format

--- elimu_yangu/careerguide/views.py
+++ elimu_yangu/careerguide/views.py
@@ -8,8 +8,6 @@ from django.core import serializers
 from django.core.serializers.json import DjangoJSONEncoder
 from elimu_yangu.careerguide.forms import InputForm
 from django.db.models import Avg
-from django.utils import translation
-from django.utils.translation import activate
 from elimu_yangu.careerguide.models import Olevel_subject_performance, Olevel_overall_performance, Alevel_subject_performance, Olevel_student_performance_2017, Alevel_student_performance, Alevel_overall_performance
 # Create your views here.
 
