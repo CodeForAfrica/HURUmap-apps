cd89217684f5b7ac0254c68099544825bd3f113c
KhadijaMahanga <khadijamahanga@y7mail.com>
2019-02-25 10:57:04 +0300

    rename dominion_ui to frontend

--- dominion/webpack.config.js
+++ dominion/webpack.config.js
@@ -1,27 +1,17 @@
 const path = require("path")
 const webpack = require('webpack')
-//const BundleTracker = require('webpack-bundle-tracker')
-//const HWP = require('html-webpack-plugin');
 
 module.exports = {
-  entry: path.join(__dirname, '/dominion_ui/src/index'), // entry point of our app. assets/js/index.js should require other js modules and dependencies it needs
+  entry: path.join(__dirname, '/frontend/src/index'), // entry point of our app. assets/js/index.js should require other js modules and dependencies it needs
 
   output: {
       path: path.resolve('./static/bundles/'),
       filename: "main.js",
   },
-
-  watch: true,
-
-  plugins: [
-  //  new BundleTracker({filename: './webpack-stats.json'}),
-  //  new HWP({template: path.join(__dirname,'/dominion/templates/homepage.html')})
-  ],
-
   module: {
     rules: [
       { test: /\.jsx?$/,
-        exclude: /node_modules/,
+        exclude: /(node_modules|bower_components)/,
         use: {
           loader: 'babel-loader'
           }
@@ -31,6 +21,6 @@ module.exports = {
 
   resolve: {
     modules: ['node_modules', 'bower_components'],
-    extensions: ['.js', '.jsx']
+    extensions: ['*','.js', '.jsx']
   },
 }
