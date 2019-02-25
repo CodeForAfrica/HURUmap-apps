57fc6ff57db7d1ad2b36c08d54f0f0f6f4d74aea
KhadijaMahanga <khadijamahanga@y7mail.com>
2019-02-25 11:27:07 +0300

    render multiple react component eg on html

---
+++ dominion/frontend/src/Nav.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default class Nav extends React.Component {
+  render() {
+    return(
+       <h2>Header menu component goes here</h2>
+    )
+  }
+}
--- dominion/frontend/src/index.js
+++ dominion/frontend/src/index.js
@@ -2,5 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
+import Nav from './Nav';
 
+ReactDOM.render(<Nav />, document.getElementById('header_menu'));
 ReactDOM.render(<App />, document.getElementById('root'));
