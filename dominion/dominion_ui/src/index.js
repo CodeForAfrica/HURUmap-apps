dcdd4af851509f1152f5ed2d8b450ec94e22b272
_ Kilemensi <kilemensi@gmail.com>
2019-02-26 14:16:37 +0300

    Add Data component to homepage

--- dominion/dominion_ui/src/index.js
+++ dominion/dominion_ui/src/index.js
@@ -5,11 +5,17 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 
 import withRoot from './withRoot';
+import Data from './components/Data';
 import Footer from './components/Footer';
 import Showcase from './components/Showcase';
 
 library.add(fab);
 
+const data = document.getElementById('dominionData');
+if (data) {
+  const DataApp = withRoot(Data);
+  ReactDOM.render(<DataApp />, data);
+}
 const showcase = document.getElementById('dominionShowcase');
 if (showcase) {
   const ShowcaseApp = withRoot(Showcase);
