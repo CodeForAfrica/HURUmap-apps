12d09ed908f853e15aaf743e21a2d1fcfd5b9dab
Karim Kawambwa <4308339+karimkawambwa@users.noreply.github.com>
2019-02-27 11:25:04 +0300

    [dominion] simplify dropdown

--- dominion/dominion_ui/src/components/Header/Dropdown.js
+++ dominion/dominion_ui/src/components/Header/Dropdown.js
@@ -1,10 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import Select from 'react-select';
 import KeyboardArrowDown from '@material-ui/icons/KeyboardArrowDown';
 
-import { Paper, TextField, Typography, MenuItem } from '@material-ui/core';
+import { Grid, Button, MenuList, MenuItem, Link } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = theme => ({
@@ -15,280 +14,103 @@ const styles = theme => ({
       paddingTop: '52px'
     }
   },
-  input: {
-    display: 'flex',
-    color: 'white'
-  },
-  valueContainer: {
-    flexGrow: 1,
-    display: 'flex',
-    flexWrap: 'nowrap',
-    alignItems: 'center',
-    backgroundColor: 'none',
+  button: {
+    padding: 0,
+    border: 0,
+    textTransform: 'none',
     [theme.breakpoints.down('xs')]: {
       width: '100%'
     }
   },
-  chip: {
-    margin: `${theme.spacing.unit / 2}px ${theme.spacing.unit / 4}px`
-  },
-  noOptionsMessage: {
-    padding: `${theme.spacing.unit}px ${theme.spacing.unit * 2}px`,
-    color: 'white'
-  },
-  singleValue: {
-    fontSize: 14,
-    color: 'white'
-  },
-  placeholder: {
-    fontSize: 14,
-    color: 'white',
+  p: {
     [theme.breakpoints.down('xs')]: {
-      width: '100%'
+      width: '100%',
+      textAlign: 'left'
     }
   },
-  paper: {
-    position: 'absolute',
-    zIndex: 2,
-    marginTop: theme.spacing.unit,
-    display: 'flex',
-    justify: 'center',
-    border: '0px solid transparent !important',
-    background: 'transparent',
-    [theme.breakpoints.up('md')]: {
-      display: 'flex',
-      flexDirection: 'column',
-      height: 'auto'
+  KeyboardArrowDown: {
+    paddingLeft: '10px',
+    cursor: 'pointer'
+  },
+  dropdownList: {
+    [theme.breakpoints.up('sm')]: {
+      display: 'none'
     }
   },
-  KeyboardArrowDown: { cursor: 'pointer' }
+  dropdownListClosed: {
+    display: 'none'
+  },
+  dropdownListItem: {
+    [theme.breakpoints.down('xs')]: {
+      paddingLeft: 0,
+      paddingRight: 0
+    }
+  },
+  link: {
+    fontSize: '30px',
+    color: '#fff',
+    textDecoration: 'none'
+  }
 });
 
-function NoOptionsMessage({ children, innerProps, selectProps }) {
-  return (
-    <Typography
-      color="textSecondary"
-      className={selectProps.classes.noOptionsMessage}
-      {...innerProps}
-    >
-      {children}
-    </Typography>
-  );
-}
+const DEFAULT_OPTIONS = ['Kenya', 'Nigeria', 'Tanzania'];
 
-NoOptionsMessage.propTypes = {
-  children: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.node),
-    PropTypes.node
-  ]).isRequired,
-  innerProps: PropTypes.shape().isRequired,
-  selectProps: PropTypes.shape().isRequired
-};
-
-function InputComponent({ inputRef, ...props }) {
-  return <div ref={inputRef} {...props} />;
-}
-
-InputComponent.propTypes = {
-  inputRef: PropTypes.shape().isRequired,
-  props: PropTypes.shape().isRequired
-};
-
-function Control({ children, innerProps, innerRef, selectProps }) {
-  return (
-    <TextField
-      InputProps={{
-        inputComponent: InputComponent,
-        inputProps: {
-          className: selectProps.classes.input,
-          inputRef: innerRef,
-          children,
-          ...innerProps
-        }
-      }}
-      {...selectProps.textFieldProps}
-    />
-  );
-}
-
-Control.propTypes = {
-  children: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.node),
-    PropTypes.node
-  ]).isRequired,
-  innerProps: PropTypes.shape().isRequired,
-  innerRef: PropTypes.shape().isRequired,
-  selectProps: PropTypes.shape().isRequired
-};
-
-function Placeholder({ children, innerProps, selectProps }) {
-  return (
-    <Typography
-      color="white"
-      className={selectProps.classes.placeholder}
-      {...innerProps}
-    >
-      {children}
-    </Typography>
-  );
-}
-
-Placeholder.propTypes = {
-  children: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.node),
-    PropTypes.node
-  ]).isRequired,
-  innerProps: PropTypes.shape().isRequired,
-  selectProps: PropTypes.shape().isRequired
-};
-
-function SingleValue({ children, innerProps, selectProps }) {
-  return (
-    <Typography className={selectProps.classes.singleValue} {...innerProps}>
-      {children}
-    </Typography>
-  );
-}
-
-SingleValue.propTypes = {
-  children: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.node),
-    PropTypes.node
-  ]).isRequired,
-  innerProps: PropTypes.shape().isRequired,
-  selectProps: PropTypes.shape().isRequired
-};
-
-function ValueContainer({ children, selectProps }) {
-  return (
-    <div className={selectProps.classes.valueContainer}>
-      {children}
-      <KeyboardArrowDown className={selectProps.classes.KeyboardArrowDown} />
-    </div>
-  );
-}
-
-ValueContainer.propTypes = {
-  children: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.node),
-    PropTypes.node
-  ]).isRequired,
-  selectProps: PropTypes.shape().isRequired
-};
-
-function Menu({ children, innerProps, selectProps }) {
-  return (
-    <Paper className={selectProps.classes.paper} {...innerProps}>
-      {children}
-    </Paper>
-  );
-}
-
-Menu.propTypes = {
-  children: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.node),
-    PropTypes.node
-  ]).isRequired,
-  innerProps: PropTypes.shape().isRequired,
-  selectProps: PropTypes.shape().isRequired
-};
-
-function Option({ children, innerProps, innerRef, isFocused }) {
-  return (
-    <MenuItem
-      buttonRef={innerRef}
-      selected={isFocused}
-      component="div"
-      style={{
-        height: '3rem',
-        color: '#fff',
-        fontWeight: 500,
-        fontSize: '2rem',
-        border: 0,
-        width: '300px',
-        padding: '1px'
-      }}
-      {...innerProps}
-    >
-      {children}
-    </MenuItem>
-  );
-}
-
-Option.propTypes = {
-  children: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.node),
-    PropTypes.node
-  ]).isRequired,
-  innerProps: PropTypes.shape().isRequired,
-  innerRef: PropTypes.shape().isRequired,
-  selectProps: PropTypes.shape().isRequired,
-  isFocused: PropTypes.bool.isRequired
-};
-
-const components = {
-  Control,
-  Menu,
-  NoOptionsMessage,
-  Option,
-  Placeholder,
-  SingleValue,
-  ValueContainer,
-  DropdownIndicator: null
-};
-
-const DEFAULT_OPTIONS = [
-  { value: 'Kenya', label: 'Kenya' },
-  { value: 'Nigeria', label: 'Nigeria' },
-  { value: 'Tanzania', label: 'Tanzania' }
-];
-
-class DropdownSelect extends React.Component {
+class Dropdown extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { single: null };
-    this.handleChange = this.handleChange.bind(this);
+    this.state = { isDropdownOpen: false };
+    this.setToggleDropdownClass = this.setToggleDropdownClass.bind(this);
   }
 
-  handleChange(city) {
-    this.setState({ single: city });
-    const { handleSearch } = this.props;
-    if (handleSearch) {
-      handleSearch(city);
-    }
+  setToggleDropdownClass() {
+    const { isDropdownOpen } = this.state;
+    this.setState({ isDropdownOpen: !isDropdownOpen });
   }
 
   render() {
-    const { classes, options, placeholder } = this.props;
-    const { single } = this.state;
-
+    const { classes, options } = this.props;
+    const { isDropdownOpen } = this.state;
     return (
-      <Select
-        className={classes.root}
-        classes={classes}
-        options={options}
-        components={components}
-        value={single}
-        onChange={this.handleChange}
-        placeholder={placeholder}
-        isSearchable="false"
-      />
+      <Grid container className={classes.root}>
+        <Button
+          disableRipple
+          className={classes.button}
+          onClick={this.setToggleDropdownClass}
+        >
+          <p className={classes.p}>Countries</p>
+          <KeyboardArrowDown
+            fontSize="large"
+            className={classes.KeyboardArrowDown}
+          />
+        </Button>
+
+        <MenuList
+          xs={12}
+          className={`${classes.dropdownList} ${
+            isDropdownOpen ? '' : classes.dropdownListClosed
+          }`}
+        >
+          {options.map(optionTitle => (
+            <MenuItem item className={classes.dropdownListItem}>
+              <Link to="/" className={classes.link} variant="body1">
+                {optionTitle}
+              </Link>
+            </MenuItem>
+          ))}
+        </MenuList>
+      </Grid>
     );
   }
 }
 
-DropdownSelect.propTypes = {
+Dropdown.propTypes = {
   classes: PropTypes.isRequired,
-  handleSearch: PropTypes.func,
-  options: PropTypes.instanceOf(Array),
-  placeholder: PropTypes.string
+  options: PropTypes.instanceOf(Array)
 };
 
-DropdownSelect.defaultProps = {
-  handleSearch: null,
-  options: DEFAULT_OPTIONS,
-  placeholder: 'Countries'
+Dropdown.defaultProps = {
+  options: DEFAULT_OPTIONS
 };
 
-export default withStyles(styles)(DropdownSelect);
+export default withStyles(styles)(Dropdown);
