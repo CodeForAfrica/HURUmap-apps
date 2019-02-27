9bb4fb8dfba48a5e76c7d93e184590021dfdcd06
Karim Kawambwa <4308339+karimkawambwa@users.noreply.github.com>
2019-02-27 15:03:11 +0300

    [dominion] initial search results

--- dominion/dominion_ui/src/components/Search/results.js
+++ dominion/dominion_ui/src/components/Search/results.js
@@ -1,11 +1,93 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Grid, List, ListItem } from '@material-ui/core';
 
-function Results() {
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    fontSize: '14px',
+    fontWeight: '500',
+    color: 'white',
+    fontFamily: theme.typography.fontFamily
+  },
+  list: {
+    width: '70%'
+  },
+  listItem: {
+    '&:hover': {
+      color: '#e7e452'
+    }
+  },
+  selected: {
+    color: '#e7e452'
+  },
+  denomination: {
+    margin: 0,
+    fontSize: '14px',
+    fontWeight: '600',
+    opacity: '0.5',
+    marginRight: '20px',
+    width: '60px',
+    textAlign: 'right'
+  },
+  name: {
+    margin: 0,
+    fontSize: '35px',
+    fontWeight: '600'
+  }
+});
+
+function Results({ classes }) {
   return (
-    <div>
-      <p style={{ color: 'white' }}>Some results</p>
+    <div className={classes.root}>
+      <Grid container direction="row" justify="flex-end">
+        <List className={classes.list}>
+          <ListItem className={classes.listItem} button>
+            <Grid container direction="row" alignItems="baseline">
+              <p className={classes.denomination}>District</p>
+              <p className={classes.name}>Mortruisberg</p>
+            </Grid>
+          </ListItem>
+          <ListItem className={classes.listItem} button>
+            <Grid container direction="row" alignItems="baseline">
+              <p className={classes.denomination}>Ward</p>
+              <p className={classes.name}>Mortruisberg</p>
+            </Grid>
+          </ListItem>
+          <ListItem className={classes.listItem} button>
+            <Grid container direction="row" alignItems="baseline">
+              <p className={classes.denomination}>Ward</p>
+              <p className={classes.name}>Mortruisberg</p>
+            </Grid>
+          </ListItem>
+          <ListItem className={classes.listItem} button>
+            <Grid container direction="row" alignItems="baseline">
+              <p className={classes.denomination}>Region</p>
+              <p className={classes.name}>Mozem</p>
+            </Grid>
+          </ListItem>
+          <ListItem className={classes.listItem} button>
+            <Grid container direction="row" alignItems="baseline">
+              <p className={classes.denomination}>Region</p>
+              <p className={classes.name}>Mortruisberg</p>
+            </Grid>
+          </ListItem>
+          <ListItem className={classes.listItem} button>
+            <Grid container direction="row" alignItems="baseline">
+              <p className={classes.denomination}>District</p>
+              <p className={classes.name}>Moertuin</p>
+            </Grid>
+          </ListItem>
+        </List>
+      </Grid>
     </div>
   );
 }
 
-export default Results;
+Results.propTypes = {
+  classes: PropTypes.shape().isRequired
+};
+
+export default withStyles(styles)(Results);
