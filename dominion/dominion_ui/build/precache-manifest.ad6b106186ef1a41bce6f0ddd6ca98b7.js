6097fb8eccba654841a8b06a9dbdb71b30ad92b3
KhadijaMahanga <khadijamahanga@y7mail.com>
2019-03-04 16:10:31 +0300

    remove media and public from gitignore

---
+++ dominion/dominion_ui/build/precache-manifest.ad6b106186ef1a41bce6f0ddd6ca98b7.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "345f88e80fb9bd0657118db934ab0ca4",
+    "url": "/static/media/databg.345f88e8.png"
+  },
+  {
+    "revision": "b9fd21e6f286d7ee6314",
+    "url": "/static/js/main.b9fd21e6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9283b78cd99417ceef3b5239f0126edb",
+    "url": "/static/media/background.9283b78c.png"
+  },
+  {
+    "revision": "330cc9ddf25f632126cad1ccfe568e90",
+    "url": "/static/media/image_4.330cc9dd.png"
+  },
+  {
+    "revision": "d4ddacefd4f13a4161bb422da60c7e9b",
+    "url": "/static/media/location.d4ddacef.svg"
+  },
+  {
+    "revision": "f1b8c0d0ca20bb265cfb928918309c94",
+    "url": "/static/media/dominion-logo.f1b8c0d0.png"
+  },
+  {
+    "revision": "ca30e81a1598789eb0c782dc55472ef3",
+    "url": "/static/media/shape.ca30e81a.svg"
+  },
+  {
+    "revision": "ad780f2833c1a06b092226b3fc1859e9",
+    "url": "/static/media/close.ad780f28.svg"
+  },
+  {
+    "revision": "32556f4944ffd8d4c44eadb403d213dc",
+    "url": "/static/media/menu.32556f49.svg"
+  },
+  {
+    "revision": "a0fb01e15e5aa7ed043bbd7cdf2100bc",
+    "url": "/static/media/back.a0fb01e1.svg"
+  },
+  {
+    "revision": "9319c5fc5ad2b24f667047f013ba4dae",
+    "url": "/static/media/land.9319c5fc.png"
+  },
+  {
+    "revision": "3b2e31dd7d825f28a42efd87beff7a34",
+    "url": "/static/media/kaitlyn-baker-422999-unsplash.3b2e31dd.png"
+  },
+  {
+    "revision": "6696e14713709b3546b07f9666718500",
+    "url": "/static/media/hero-image-1.6696e147.png"
+  },
+  {
+    "revision": "615560811b64d44e58ae0be4e61f49ca",
+    "url": "/static/media/hero_bg.61556081.png"
+  },
+  {
+    "revision": "a2c0a988e0381df8ba1b1771ee386f43",
+    "url": "/static/media/combined-shape.a2c0a988.svg"
+  },
+  {
+    "revision": "3dc6948ee82f44701a3b70b0fc2dc450",
+    "url": "/static/media/hero_map.3dc6948e.png"
+  },
+  {
+    "revision": "fe7081689387c86358c5d8677f7165ab",
+    "url": "/static/media/smallscreen_background.fe708168.png"
+  },
+  {
+    "revision": "c68bf4105d5fae42837a4178f558a2fe",
+    "url": "/static/media/bmgf.c68bf410.png"
+  },
+  {
+    "revision": "bda4db8471f33ed14b4b2e2fd8a420c3",
+    "url": "/static/media/codeforafrica.bda4db84.png"
+  },
+  {
+    "revision": "37dfe5cb4541196b2a0a1d0591245e47",
+    "url": "/static/media/icfj.37dfe5cb.png"
+  },
+  {
+    "revision": "bb78f01e6b9c6482ffc101f0052327e9",
+    "url": "/static/media/twaweza.bb78f01e.png"
+  },
+  {
+    "revision": "0c3cbd5b40e8bd7b65cbee495c1d499f",
+    "url": "/static/media/image.0c3cbd5b.png"
+  },
+  {
+    "revision": "17242cbd12eab1f86a2ae210e3b440a4",
+    "url": "/static/media/image_2.17242cbd.png"
+  },
+  {
+    "revision": "91f515953207a04400cddcf74389302f",
+    "url": "/static/media/image_3.91f51595.png"
+  },
+  {
+    "revision": "13f327e8d1f74b109ad9",
+    "url": "/static/js/1.13f327e8.chunk.js"
+  },
+  {
+    "revision": "7be4e1ee4755f36db1632ff94d07fba4",
+    "url": "/index.html"
+  }
+];
