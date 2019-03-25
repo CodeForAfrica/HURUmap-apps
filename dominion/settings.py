80ab2d59b67ad7af03cf4f91af0ededf0ae5d10f
_ Kilemensi <kilemensi@gmail.com>
2019-03-25 11:42:35 +0300

    Order release years

--- dominion/settings.py
+++ dominion/settings.py
@@ -142,7 +142,7 @@ HURUMAP['latest_release_year'] = 'latest'
 
 # default census release years
 HURUMAP['available_release_years'] = {
-    'country': [2009, 2017, 2018, 2016]
+    'country': [2009, 2016, 2017, 2018]
 }
 
 # census release years for each country and it's subnational geographies
