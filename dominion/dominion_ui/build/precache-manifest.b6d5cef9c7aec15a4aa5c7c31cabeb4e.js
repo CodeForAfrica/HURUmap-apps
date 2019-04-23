daa47c60be270864560bd4e49370982ef1c5613e
David Lemayian <dlemayian@gmail.com>
2019-04-23 14:25:08 +0300

    Add Dominion build folder to Git

---
+++ dominion/dominion_ui/build/precache-manifest.b6d5cef9c7aec15a4aa5c7c31cabeb4e.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca30e81a1598789eb0c782dc55472ef3",
+    "url": "/static/media/shape.ca30e81a.svg"
+  },
+  {
+    "revision": "92678822757511674ff9",
+    "url": "/static/js/main.92678822.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a0fb01e15e5aa7ed043bbd7cdf2100bc",
+    "url": "/static/media/back.a0fb01e1.svg"
+  },
+  {
+    "revision": "d4ddacefd4f13a4161bb422da60c7e9b",
+    "url": "/static/media/location.d4ddacef.svg"
+  },
+  {
+    "revision": "9283b78cd99417ceef3b5239f0126edb",
+    "url": "/static/media/background.9283b78c.png"
+  },
+  {
+    "revision": "38a29f899ad3bdec47c4ea506f94cca6",
+    "url": "/static/media/hero-image-3_2.38a29f89.png"
+  },
+  {
+    "revision": "bda4db8471f33ed14b4b2e2fd8a420c3",
+    "url": "/static/media/codeforafrica.bda4db84.png"
+  },
+  {
+    "revision": "bb78f01e6b9c6482ffc101f0052327e9",
+    "url": "/static/media/twaweza.bb78f01e.png"
+  },
+  {
+    "revision": "345f88e80fb9bd0657118db934ab0ca4",
+    "url": "/static/media/databg.345f88e8.png"
+  },
+  {
+    "revision": "fb06e88b99f776081a0059b6f7ec7088",
+    "url": "/static/media/hero-image-3.fb06e88b.png"
+  },
+  {
+    "revision": "f1b8c0d0ca20bb265cfb928918309c94",
+    "url": "/static/media/dominion-logo.f1b8c0d0.png"
+  },
+  {
+    "revision": "335659fb24d16dad323a2b44a8c1ae3d",
+    "url": "/static/media/dominion-logo-country.335659fb.png"
+  },
+  {
+    "revision": "6696e14713709b3546b07f9666718500",
+    "url": "/static/media/hero-image-1.6696e147.png"
+  },
+  {
+    "revision": "ad780f2833c1a06b092226b3fc1859e9",
+    "url": "/static/media/close.ad780f28.svg"
+  },
+  {
+    "revision": "32556f4944ffd8d4c44eadb403d213dc",
+    "url": "/static/media/menu.32556f49.svg"
+  },
+  {
+    "revision": "a2c0a988e0381df8ba1b1771ee386f43",
+    "url": "/static/media/combined-shape.a2c0a988.svg"
+  },
+  {
+    "revision": "615560811b64d44e58ae0be4e61f49ca",
+    "url": "/static/media/hero_bg.61556081.png"
+  },
+  {
+    "revision": "fe7081689387c86358c5d8677f7165ab",
+    "url": "/static/media/smallscreen_background.fe708168.png"
+  },
+  {
+    "revision": "3dc6948ee82f44701a3b70b0fc2dc450",
+    "url": "/static/media/hero_map.3dc6948e.png"
+  },
+  {
+    "revision": "3b2e31dd7d825f28a42efd87beff7a34",
+    "url": "/static/media/kaitlyn-baker-422999-unsplash.3b2e31dd.png"
+  },
+  {
+    "revision": "d78e63d8f18c30559e27a328f98db40c",
+    "url": "/static/media/black-combined-shape.d78e63d8.svg"
+  },
+  {
+    "revision": "c68bf4105d5fae42837a4178f558a2fe",
+    "url": "/static/media/bmgf.c68bf410.png"
+  },
+  {
+    "revision": "37dfe5cb4541196b2a0a1d0591245e47",
+    "url": "/static/media/icfj.37dfe5cb.png"
+  },
+  {
+    "revision": "2034513b31b3121ff479",
+    "url": "/static/js/1.2034513b.chunk.js"
+  },
+  {
+    "revision": "67ddb29d2f8b65325e5abb43113d466c",
+    "url": "/index.html"
+  }
+];
