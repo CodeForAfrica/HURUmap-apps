3987546e5cdbe961cd20d72e3c527ea8c7b5399a
Clemence Kyara <clemence@codeforafrica.org>
2019-06-18 13:25:48 +0300

    Revert loaddata changes

--- contrib/loaddata.sh
+++ contrib/loaddata.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ -z ${HURUMAP_APP+x} ]; then HURUMAP_APP='hurumap_ng'; fi
+if [ -z ${HURUMAP_APP+x} ]; then HURUMAP_APP='hurumap_land'; fi
 
 cat ${HURUMAP_APP}/sql/*.sql | psql            # Load tables / data into DB (TBD)
 python manage.py loaddata ${HURUMAP_APP}/fixtures/${HURUMAP_APP}.json  # Load fixtures
