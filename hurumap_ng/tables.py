460da6b415afd51ea9cc0cd246f56a69ef0e9be7
khadija <khadijamahanga@y7mail.com>
2019-06-26 06:41:24 +0300

    update table

--- hurumap_ng/tables.py
+++ hurumap_ng/tables.py
@@ -2,6 +2,28 @@ from wazimap.data.tables import FieldTable
 
 FieldTable(fields=['year', 'debt_type'], id='external_domestic_debt', year=2018, dataset='National Bureau of Statistics')
 
-FieldTable(fields=['year'], id='arrested_suspects', year=2018, dataset='National Bureau of Statistics')
+FieldTable(fields=['year'], id='arrested_suspects', year=2016, dataset='National Bureau of Statistics')
 
-FieldTable(fields=['year'], id='drugs_seized', year=2018, dataset='National Bureau of Statistics')
+FieldTable(fields=['year'], id='drugs_seized', year=2016, dataset='National Bureau of Statistics')
+
+FieldTable(fields=['year'], id='conviction_secured', year=2016, dataset='National Bureau of Statistics')
+
+FieldTable(fields=['year'], id='counselling_concluded', year=2016, dataset='National Bureau of Statistics')
+
+FieldTable(fields=['year'], id='suspects_prosecuted', year=2016, dataset='National Bureau of Statistics')
+
+FieldTable(fields=['year'], id='mineral_production', year=2018, dataset='National Bureau of Statistics')
+
+FieldTable(fields=['period', 'subscription_type'], id='telecom_subscription', year=2018, dataset='National Bureau of Statistics')
+
+FieldTable(fields=['network', 'subscription_type'], id='mobile_subscription', year=2018, dataset='National Bureau of Statistics')
+
+FieldTable(fields=['year', 'gender'], id='jamb', year=2018, dataset='National Bureau of Statistics')
+
+FieldTable(fields=['allocation'], id='faac', year=2018, dataset='National Bureau of Statistics')
+
+FieldTable(fields=['gender', 'year'], id='hiv_patients', year=2018, dataset='National Bureau of Statistics')
+
+FieldTable(fields=['month, year'], id='diesel_price', year=2018, dataset='National Bureau of Statistics')
+
+FieldTable(fields=['month', 'year'], id='petrol_price', year=2018, dataset='National Bureau of Statistics')
