d892e71e8d10b088bccac40d14db8691db38d426
khadija <khadijamahanga@y7mail.com>
2019-06-27 06:42:10 +0300

    update sql files

--- hurumap_ng/sql/food_price.sql
+++ hurumap_ng/sql/food_price.sql
@@ -13,17 +13,17 @@ SELECT pg_catalog.set_config('search_path', '', false);
 SET check_function_bodies = false;
 SET client_min_messages = warning;
 
-ALTER TABLE IF EXISTS ONLY public.faac DROP CONSTRAINT IF EXISTS faac_pkey;
-DROP TABLE IF EXISTS public.faac;
+ALTER TABLE IF EXISTS ONLY public.food_price DROP CONSTRAINT IF EXISTS food_price_pkey;
+DROP TABLE IF EXISTS public.food_price;
 SET default_tablespace = '';
 
 SET default_with_oids = false;
 
 --
--- Name: faac; Type: TABLE; Schema: public; Owner: -
+-- Name: food_price; Type: TABLE; Schema: public; Owner: -
 --
 
-CREATE TABLE public.faac (
+CREATE TABLE public.food_price (
     geo_level character varying(15) NOT NULL,
     geo_code character varying(10) NOT NULL,
     geo_version character varying(128) NOT NULL,
@@ -33,20 +33,20 @@ CREATE TABLE public.faac (
 
 
 --
--- Data for Name: faac; Type: TABLE DATA; Schema: public; Owner: -
+-- Data for Name: food_price; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-COPY public.faac (geo_level, geo_code, geo_version, year, total) FROM stdin;
+COPY public.food_price (geo_level, geo_code, geo_version, year, total) FROM stdin;
 
 \.
 
 
 --
--- Name: faac faac_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+-- Name: food_price food_price_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
-ALTER TABLE ONLY public.faac
-    ADD CONSTRAINT faac_pkey PRIMARY KEY (geo_level, geo_code, geo_version, year);
+ALTER TABLE ONLY public.food_price
+    ADD CONSTRAINT food_price_pkey PRIMARY KEY (geo_level, geo_code, geo_version, year);
 
 
 --
